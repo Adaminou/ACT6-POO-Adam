b96aff0a8c192afa69b7caf92697a6315b6a809f
Essalhi Adam <200537@CCM-Asty-Moulin.local>
2024-01-09 14:43:05 +0100

    fini

---
+++ EX1_Vehicules_Adam/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EX1_Vehicules_Adam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
