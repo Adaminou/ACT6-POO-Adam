96894fa06acc867d0130540b0fca2750ae54f937
Essalhi Adam <200537@CCM-Asty-Moulin.local>
2024-01-16 12:51:34 +0100

    FINITO

--- EX 1/EX1_Vehicules_Adam/Program.cs
+++ EX 1/EX1_Vehicules_Adam/Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace EX1_Vehicules_Adam
 {
-    internal class Program
+     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Vehicule monVehicule = new Vehicule("Dodge","Charger","Noir", 30000);
+            Console.WriteLine(monVehicule.Affiche());
+            Velo monVelo = new Velo("VTT",false,"Jsp","Decathlon","Noir",450);
+            Console.WriteLine(monVelo.Affiche());
+            Voiture MaVoiture = new Voiture("disel",true,"Dodge","Charger","Noir",30000);
+            Console.WriteLine(MaVoiture.Affiche());
         }
     }
 }
--- EX 1/EX1_Vehicules_Adam/Vehicule.cs
+++ EX 1/EX1_Vehicules_Adam/Vehicule.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace EX1_Vehicules_Adam
 {
-    internal class MethodeDeProjet
+     class Vehicule
     {
+        protected string _modele;
+        protected string _marque;
+        protected string _couleur;
+        protected int _prix;
+
+        public string Modele
+        {
+            get { return _modele; }
+            set { _modele = value; }
+        }
+
+        public string Marque
+        {
+            get { return _marque; }
+            set { _marque = value; }
+        }
+
+        public string Couleur
+        {
+            get { return _couleur; }
+            set { _couleur = value; }
+        }
+
+        public int Prix
+        {
+            get { return _prix; }
+            set { _prix = value; }
+        }
+
+        public Vehicule(string modele, string marque, string couleur, int prix)
+        {
+            _modele = modele;
+            _marque = marque;
+            _couleur = couleur;
+            _prix = prix;
+        }
+
+        public string Affiche()
+        {
+            string info = "le modele du véhicule : " + Modele + "La marque du véhicule : " + Marque + " la couleur du véhicule : " + Couleur + "son prix est de : " + Prix;
+            return info;
+        }
     }
+}
 }
